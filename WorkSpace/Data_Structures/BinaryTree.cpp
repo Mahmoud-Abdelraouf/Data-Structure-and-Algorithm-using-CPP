b426b250b17fc24ad3857d24197e5afcf2458f76
Mahmoud <mahmoud126125@gmail.com>
2023-12-30 20:20:56 +0200

    Solve the issues of Copy/Paste

--- WorkSpace/Data_Structures/BinaryTree.cpp
+++ WorkSpace/Data_Structures/BinaryTree.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
 template <typename T>
 class BinaryTree {
 private:
@@ -25,12 +30,36 @@ public:
         right = newRight;
     }
 
-    BinaryTree *getLeftNode() const {
+    T getData(void) const{
+        return data;
+    }
+
+    BinaryTree *getLeftNode(void) const {
         return left;
     }
 
-    BinaryTree *getRightNode() const {
+    BinaryTree *getRightNode(void) const {
         return right;
+    }
+
+    void printTree(const BinaryTree *root) const {
+        if(root == nullptr) {
+            return;
+        }
+
+        cout << root->data << ": ";
+        if(root->left != nullptr) {
+            cout << "L: " << root->left->data << ", ";
+        } 
+
+        if(root->right != nullptr) {
+            cout << "R: " << root->right->data;
+        }
+
+        cout << endl;
+
+        printTree(root->left);
+        printTree(root->right);
     }
 
     ~BinaryTree() {
@@ -41,14 +70,133 @@ public:
 };
 
 
+template <typename T>
+BinaryTree<T>* createBinaryTree() {
+    T data;
+    std::cout << "Enter data: ";
+    std::cin >> data;
+
+    if (data == -1) {
+        return nullptr;
+    }
+
+    BinaryTree<T>* newNode = new BinaryTree<T>(data);
+
+    std::cout << "Enter left child of " << data << ":\n";
+    newNode->setLeftNode(createBinaryTree<T>());
+
+    std::cout << "Enter right child of " << data << ":\n";
+    newNode->setRightNode(createBinaryTree<T>());
+
+    return newNode;
+}
+
+template <typename T>
+BinaryTree<T>* createBinaryTreeLevelWise() {
+    std::queue<BinaryTree<T>*> nodeQueue;
+    T data;
+    std::cout << "Enter root data: ";
+    std::cin >> data;
+
+    if (data == -1) {
+        return nullptr;
+    }
+
+    BinaryTree<T>* root = new BinaryTree<T>(data);
+    nodeQueue.push(root);
+
+    while (!nodeQueue.empty()) {
+        BinaryTree<T>* currentNode = nodeQueue.front();
+        nodeQueue.pop();
+
+        T leftData, rightData;
+        std::cout << "Enter left child of " << currentNode->getData() << ": ";
+        std::cin >> leftData;
+
+        if (leftData != -1) {
+            currentNode->setLeftNode(new BinaryTree<T>(leftData));
+            nodeQueue.push(currentNode->getLeftNode());
+        }
+
+        std::cout << "Enter right child of " << currentNode->getData() << ": ";
+        std::cin >> rightData;
+
+        if (rightData != -1) {
+            currentNode->setRightNode(new BinaryTree<T>(rightData));
+            nodeQueue.push(currentNode->getRightNode());
+        }
+    }
+
+    return root;
+}
+
+template <typename T>
+vector<vector<T>> levelOrder(const BinaryTree<T>* root) {
+    vector<vector<T>> result;
+    if (root == nullptr) {
+        return result;
+    }
+
+    queue<const BinaryTree<T>*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size(); // Size of nodes at current level
+        vector<T> currentLevel;
+
+        // Process all nodes in the current level
+        while (levelSize > 0) {
+            const BinaryTree<T>* node = q.front();
+            q.pop();
+            currentLevel.push_back(node->getData());
+
+            if (node->getLeftNode()) {
+                q.push(node->getLeftNode());
+            }
+            if (node->getRightNode()) {
+                q.push(node->getRightNode());
+            }
+
+            levelSize--;
+        }
+
+        result.push_back(currentLevel);
+    }
+
+    return result;
+}
+
 int main() {
+/**  
+ *   BinaryTree<int> *root = new BinaryTree<int> (5);
+ *   BinaryTree<int> *left = new BinaryTree<int> (7);
+ *   BinaryTree<int> *right = new BinaryTree<int> (10);
+ *
+ *   root->setLeftNode(left);
+ *   root->setRightNode(right);
+ */
 
-    BinaryTree<int> *root = new BinaryTree<int> (5);
-    BinaryTree<int> *left = new BinaryTree<int> (7);
-    BinaryTree<int> *right = new BinaryTree<int> (10);
+/**
+ * BinaryTree<int> *root = createBinaryTree<int>();
+ */
 
-    root->setLeftNode(left);
-    root->setRightNode(right);
+/**
+ * BinaryTree<int> *root = createBinaryTreeLevelWise<int>();
+ * root->printTree(root);
+*/
+    BinaryTree<int> *root = createBinaryTreeLevelWise<int>();
+
+    // Performing level-order traversal
+    vector<vector<int>> traversalResult = levelOrder(root);
+
+    // Displaying the traversal result
+    cout << "Level-order traversal result:" << endl;
+    for (const auto& level : traversalResult) {
+        for (const auto& node : level) {
+            cout << node << " ";
+        }
+        cout << endl;
+    }
 
     // Deleting the root node will trigger the destructor to recursively delete the entire tree
     delete root;
